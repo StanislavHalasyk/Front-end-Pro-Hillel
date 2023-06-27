26711d47b26545b163a0d74e9ae3b659eaef2e93
Stanislav Halasyk <galasik_stanislav@icloud.com>
2023-06-27 13:52:59 +0300

    homework

---
+++ homework_1/index.js
@@ -0,0 +1,76 @@
+//String
+
+                                    //Можно было сразу записать все в const и не указывать дополнительные переменные let:
+                                                                    // const aStr = String(true);
+                                                                   // console.log(typeof aStr); 
+let a = true;                   
+const aStr = String(a);
+console.log(typeof aStr);     
+
+let b = 1000;
+const bStr = String(b);
+console.log(typeof bStr);  
+
+let c = null;
+const cStr = String(c);
+console.log(typeof cStr);
+
+//Number
+
+let d = '123a';
+const dNum = Number(d);
+console.log(typeof dNum);    //Это значение NaN. Можно выделить отдельно число через parseInt 
+                            // let d = '123a';
+                            // const dNum  = parseInt(d);
+                            // console.log (dNum)
+
+
+let e = '1000';
+const eNum = Number(e);
+console.log(typeof eNum);    
+
+let f = 'null';
+const fNum = Number(d);
+console.log(typeof fNum);
+
+let g = 'undefined';
+const gNum = Number(g);
+console.log(typeof gNum);
+
+let h = 'false';
+const hNum = Number(h);
+console.log(typeof hNum);
+
+
+//Boolean
+
+
+let i = null;
+const iBoolean = Boolean(i); 
+console.log(typeof iBoolean);   //Это у нас False
+
+let j = '';
+const jBoolean = Boolean(j) ;
+console.log(typeof jBoolean);   //Это у нас False
+
+let k = 0;
+const kBoolean = Boolean(k) ;
+console.log(typeof kBoolean);   //Это у нас False
+
+let l = ' ';
+const lBoolean = Boolean(l) ;
+console.log(typeof lBoolean);   //Это у нас True
+
+let m = 1000;
+const mBoolean = Boolean(m) ;
+console.log(typeof mBoolean);   //Это у нас True
+
+let n = '1200';
+const nBoolean = Boolean(n) ;
+console.log(typeof nBoolean);   //Это у нас True
+
+
+
+
+
+
---
+++ homework_2/index.js
@@ -0,0 +1,4 @@
+let width = prompt ('Введите ширину блока в пикселях (Пример ввода 100px)');
+const widthNum = parseInt(width);
+console.log (widthNum);
+console.log (typeof widthNum);
