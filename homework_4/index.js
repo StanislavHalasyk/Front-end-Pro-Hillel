5f18835619b8a87ab874de67dfa503cf5d8643f2
Stanislav Halasyk <galasik_stanislav@icloud.com>
2023-06-28 19:46:23 +0300

    homework4

--- homework_4/index.js
+++ homework_4/index.js
@@ -1,8 +1,33 @@
+
 const text = prompt ('Введите произвольный текст');
 const maxLength = +prompt ('Введите сколько симфолов Вы хотите видеть');
 
 const textMaxLength = text.slice(0, maxLength);
+let result;
 
-const textFinal = textMaxLength.concat('…');
-console.log (textFinal);
+if (text.length > maxLength ) {
+    result = textMaxLength.concat('…');
+} else if(text.length < maxLength){
+    result = text;
+}
 
+console.log (result);
+
+
+//Вопрос. Стоит ли задавать сразу переменную textMaxLength ? Это не будет считаться лишним действием ? Так как если условие не выполняется 
+// ( строка которую ввел пользователь короче чем длина которую он указал) и это действие просто пропускается и выполняется сразу else if?
+
+// const text = prompt ('Введите произвольный текст');
+// const maxLength = +prompt ('Введите сколько симфолов Вы хотите видеть');
+
+// let textMaxLength;
+// let result;
+
+// if (text.length > maxLength ) {
+//     textMaxLength = text.slice(0, maxLength)
+//     result = textMaxLength.concat('…');
+// } else if(text.length < maxLength){
+//     result = text;
+// }
+
+// console.log (result);
