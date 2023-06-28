2fc4348d82970998366855f229c4161dbf3c7760
Stanislav Halasyk <galasik_stanislav@icloud.com>
2023-06-28 18:31:04 +0300

    homework5

---
+++ homework_5/index.js
@@ -0,0 +1,18 @@
+let quastion = prompt('Введите произвольный текст');
+
+if (quastion.includes('free')){
+    console.log('Это спам');
+} else {
+    console.log('Это не спам')
+};
+
+
+
+// let quastion = prompt('Введите произвольный текст');
+// let result;
+
+// if (quastion.includes('free')){
+//   result = 'Это спам';
+// } else {
+    // result = 'Это не спам'};
+// console.log (result);
