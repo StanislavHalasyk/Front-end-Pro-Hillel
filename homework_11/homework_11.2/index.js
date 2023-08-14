adb35922c2131d19a35fc25db684d2841108d067
Stanislav Halasyk <galasik_stanislav@icloud.com>
2023-08-14 11:41:37 +0300

    homework12

--- homework_11/homework_11.2/index.js
+++ homework_11/homework_11.2/index.js
@@ -1,13 +1,13 @@
 for (let i = 2; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i)
+    if (i % 2 == 0) {
+      console.log(i)
+    }
   }
-}
-
-// let isNumber = null;
-// for (let i = 2; i <= 10; i++) {
-//   if (i % 2 == 0) {
-//     isNumber = i;
-//     console.log(isNumber)
-//   }
-// }
+  
+  // let isNumber = null;
+  // for (let i = 2; i <= 10; i++) {
+  //   if (i % 2 == 0) {
+  //     isNumber = i;
+  //     console.log(isNumber)
+  //   }
+  // }
---
+++ homework_12/index.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll(".counter__btn");  
+ 
+btns.forEach( btn=> {
+    btn.addEventListener('click', function () {
+        const direction = this.dataset.direction;
+        const input = this.parentElement.querySelector(".counter__value");
+        const currentValue = +input.value; 
+        let newValue;
+
+        switch(direction){
+            case 'plus':
+            newValue = currentValue + 1;  
+            break;  
+            case 'minus':
+            newValue = currentValue - 1; 
+            break;
+            case 'reset':
+            newValue = 0; 
+            break;
+            default:
+            newValue;
+        }    
+   
+        input.value = newValue;
+    })
+})
